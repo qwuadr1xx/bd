14cd11f05e966264979817a366c00e6d6dc775ae
qwuadrixx <qwuadrixx@gmail.com>
2026-05-04 10:35:35 +0300

    mid

--- lab3/trigger.sql
+++ lab3/trigger.sql
@@ -1,30 +1,46 @@
 CREATE OR REPLACE FUNCTION check_and_calculate_distance()
     RETURNS TRIGGER AS
-$$
+$function$
 DECLARE
     calculated_distance bigint;
-    current_distance    bigint;
+    spaceships_type     spaceship_type;
 BEGIN
     SELECT ROUND(SQRT(
-                         POWER(dest.x - orig.x, 2) +
-                         POWER(dest.y - orig.y, 2) +
-                         POWER(dest.z - orig.z, 2)
-                 ) * 1.25)::BIGINT
+            POWER(dest.x - orig.x, 2) +
+            POWER(dest.y - orig.y, 2) +
+            POWER(dest.z - orig.z, 2)
+                 ))::BIGINT
     INTO calculated_distance
     FROM coordinates orig,
          coordinates dest
     WHERE orig.id = NEW.origin_coordinates_id
       AND dest.id = NEW.destination_coordinates_id;
 
-    SELECT t.distance FROM trajectory t INTO current_distance;
+    SELECT type
+    INTO spaceships_type
+    FROM spaceships
+    WHERE id = NEW.spaceship_id
+    LIMIT 1;
 
-    IF calculated_distance < NEW.distance THEN calculated_distance = NEW.distance; END IF;
+    IF spaceships_type = liner THEN
+        calculated_distance = calculated_distance * 1.1;
+    ELSEIF spaceships_type = dreadnought THEN
+        calculated_distance = calculated_distance * 2;
+    ELSEIF spaceships_type = commercial THEN
+        calculated_distance = calculated_distance * 1.5;
+    ELSE
+        calculated_distance = calculated_distance * 1.25;
+    END IF;
+
+    IF calculated_distance < NEW.distance THEN
+        calculated_distance = NEW.distance;
+    END IF;
     NEW.distance = calculated_distance;
     return NEW;
 END;
-$$ language plpgsql;
+$function$ LANGUAGE plpgsql;
 
-CREATE TRIGGER check_and_calculate_distance
+CREATE OR REPLACE TRIGGER check_and_calculate_distance
     BEFORE INSERT OR UPDATE OF origin_coordinates_id, destination_coordinates_id, distance
     ON trajectory
     FOR EACH ROW
